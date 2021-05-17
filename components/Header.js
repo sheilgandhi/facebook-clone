9fbfd739ca49b77c3cd5a5a3b359f0d463e7ca97
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-05-17 22:35:52 +1200

    basic setup

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+function Header() {
+    return (
+        <div>
+            <h1>Header</h1>
+            
+            {/* Left */}
+            <div>
+                <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
+            </div>
+
+            {/* Center */}
+
+            {/* Right */}
+        </div>
+    )
+}
+
+export default Header
---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        domains: [
+            "links.papareact.com",
+            "platform-lookaside.fbsbx.com", // facebooks photos
+            "firebasestorage.googleapis.com", // firebase
+        ],
+    }
+}
