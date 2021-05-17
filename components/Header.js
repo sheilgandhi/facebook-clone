3c15e92502f4cf2f9a13340c48597a43b9043d8f
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-05-17 23:35:29 +1200

    Header built still requires Firebase elements

--- components/Header.js
+++ components/Header.js
@@ -11,14 +11,14 @@ function Header() {
                 <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2" >
                     <SearchIcon className='h-6 text-gray-600' />
-                    <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook"/>
+                    <input className=" hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook"/>
                 </div>
             </div>
 
             {/* Center */}
             <div className="flex justify-center flex-grow">
                 <div className="flex space-x-6 md:space-x-2">
-                    <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon active Icon={HomeIcon} />
                     <HeaderIcon Icon={FlagIcon} />
                     <HeaderIcon Icon={PlayIcon} />
                     <HeaderIcon Icon={ShoppingCartIcon} />
@@ -27,6 +27,14 @@ function Header() {
             </div>
 
             {/* Right */}
+            <div className="flex items-center sm:space-x-2 justify-end">
+                {/* <Image src={} /> Profile Pic */}
+                <p className=" whitespace-nowrap semi-bold ">Sheil Gandhi</p>
+                <ViewGridIcon className="icon" />
+                <ChatIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChevronDownIcon className="icon" />
+            </div>
         </div>
     )
 }
