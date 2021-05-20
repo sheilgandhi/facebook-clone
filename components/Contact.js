50dce0648323229612f10214296c903ac74e3dba
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-05-20 20:38:27 +1200

    Making Widgets

---
+++ components/Contact.js
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+function Contact({ src, name }) {
+    return (
+        <div className="">
+            <Image className="rounded-full" objectFit="contain" src={src} width={50} height={50} layout="fixed" />
+            <p>{name}</p>
+        </div>
+    )
+}
+
+export default Contact
