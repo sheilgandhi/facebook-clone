cf80a2b46be32160249b4954cedb91cabd7dc3c9
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-05-20 18:39:10 +1200

    Photo/Video is added to Firebase Stroage

--- components/InputBox.js
+++ components/InputBox.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 import { EmojiHappyIcon } from '@heroicons/react/outline'
 import { CameraIcon, VideoCameraIcon } from '@heroicons/react/solid'
 import { useRef, useState } from "react"
-import { db } from "../firebase"
+import { db, storage } from "../firebase"
 import firebase from 'firebase'
 
 
@@ -40,7 +40,23 @@ function InputBox() {
             email: session.user.email,
             image: session.user.image,
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        });
+        }).then(
+            doc => {
+                if(imageToPost){
+                    const uploadTask = storage.ref(`posts/${doc.id}`).putString(imageToPost, 'data_url');
+                    removeImage();
+
+                    uploadTask.on('state_change', null, error => console.error(error), () => {
+                        // when upload completes
+                        storage.ref(`posts`).child(doc.id).getDownloadURL().then(url => {
+                            db.collection('posts').doc(doc.id).set({
+                                postImage: url,
+                            }, { merge: true })
+                        })
+                    })
+                }
+            }
+        )
 
         inputRef.current.value = "";
     }
